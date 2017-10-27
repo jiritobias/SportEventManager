df1e5770aeaa44aa07224f9f87c6e4a31e6a7a93
tobi.jiri <tobi.jiri@gmail.com>
2017-10-27 22:09:53 +0200

    base test update

--- persistence/src/test/java/cz/fi/muni/pa165/dao/BaseDaoImplTest.java
+++ persistence/src/test/java/cz/fi/muni/pa165/dao/BaseDaoImplTest.java
@@ -1,11 +1,19 @@
 package cz.fi.muni.pa165.dao;
 
 import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
+import cz.fi.muni.pa165.entity.Sport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.Test;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * @author jiritobias
@@ -15,4 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class BaseDaoImplTest extends AbstractTestNGSpringContextTests {
 
+    @Qualifier("sportDaoImpl")
+    @Autowired
+    BaseDao<Sport> baseDao;
+
+    @Test
+    @Transactional
+    public void createTest() {
+        Sport sport = new Sport();
+        sport.setName("First");
+
+        baseDao.create(sport);
+
+        List<Sport> all = baseDao.findAll();
+        assertTrue(all.contains(sport));
+    }
 }
