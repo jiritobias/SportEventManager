7824d4cc05ef29b012e6f949988c95ca91a951f8
Martin Šmíd <martin.smid94@seznam.cz>
2017-10-27 21:09:04 +0200

    Add sportsmen tests

--- persistence/src/test/java/cz/fi/muni/pa165/dao/SportsMenDaoImplTest.java
+++ persistence/src/test/java/cz/fi/muni/pa165/dao/SportsMenDaoImplTest.java
@@ -49,6 +49,7 @@ public class SportsMenDaoImplTest extends BaseDaoImplTest {
 
         User found = sportsMenDao.findById(sportsmen.getId());
         assertNotNull(found);
+        assertEquals(found, sportsmen);
     }
 
     @Test
@@ -60,30 +61,57 @@ public class SportsMenDaoImplTest extends BaseDaoImplTest {
         sportsMenDao.addToCompetition(competition, sportsmen);
 
         Competition foundCompetition = competitionDao.findById(competition.getId());
-        Assert.assertEquals();
         Set<User> sportsmenInComp = foundCompetition.getSportsMen();
+
+        Assert.assertTrue(sportsmen.getCompetitions().contains(competition));
+        Assert.assertEquals(sportsmenInComp.size(), 1);
         Assert.assertTrue(sportsmenInComp.contains(sportsmen));
     }
 
     @Test
-    public void testRemoveFromCompetition() throws Exception {
+    public void testRemoveFromCompetition() {
+        Competition competition = new Competition();
+        competitionDao.create(competition);
+        Competition competition1 = new Competition();
+        competitionDao.create(competition1);
+
+        sportsMenDao.create(sportsmen);
+
+        sportsMenDao.removeFromCompetition(competition, sportsmen);
+        Assert.assertEquals(sportsMenDao.findById(sportsmen.getId()).getCompetitions().size(), 0);
+
+        sportsMenDao.addToCompetition(competition, sportsmen);
+        sportsMenDao.addToCompetition(competition1, sportsmen);
+        sportsMenDao.removeFromCompetition(competition, sportsmen);
+
+        Competition found = competitionDao.findById(competition.getId());
+        Assert.assertNull(found);
+        Assert.assertEquals(sportsmen.getCompetitions().size(), 1);
+        found = competitionDao.findById(competition1.getId());
+        Assert.assertNotNull(found);
+        Set<User> sportsmenInComp = found.getSportsMen();
+
+        Assert.assertTrue(sportsmen.getCompetitions().contains(competition));
+        Assert.assertEquals(sportsmenInComp.size(), 1);
+        Assert.assertTrue(sportsmenInComp.contains(sportsmen));
+    }
+
+    @Test
+    public void testAddToTeam() {
 
     }
 
     @Test
-    public void testAddToTeam() throws Exception {
-    }
-
-    @Test
-    public void testRemoveFromTeam() throws Exception {
+    public void testRemoveFromTeam() {
 
     }
 
     @Test
-    public void testFindAll() throws Exception {
+    public void testFindAll() {
         sportsMenDao.create(sportsmen);
 
         List<User> found = sportsMenDao.findAll();
-        assertEquals( found.size(), 1);
+        assertEquals(found.size(), 1);
+        assertEquals(found.get(0), sportsmen);
     }
 }
