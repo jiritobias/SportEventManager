6f37e92089f63ea3cf823b455c1e9bd3cc5a841b
Martin Šmíd <martin.smid94@seznam.cz>
2017-11-17 17:28:38 +0100

    Add sport dto

--- api-layer/src/main/java/cz/fi/muni/pa165/dto/SportDTO.java
+++ api-layer/src/main/java/cz/fi/muni/pa165/dto/SportDTO.java
@@ -1,11 +1,31 @@
 package cz.fi.muni.pa165.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
- * @author jiritobias
+ * @author jiritobias, Martin Smid
  */
 @Getter
+@Setter
 public class SportDTO {
+
     private Long id;
+
+    private String name;
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SportDTO sportDTO = (SportDTO) o;
+
+        return name != null ? name.equals(sportDTO.name) : sportDTO.name == null;
+    }
 }
