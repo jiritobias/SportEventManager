2dc517ae85254c434a9680b1633ae4c93329478c
Martin Šmíd <martin.smid94@seznam.cz>
2017-11-19 20:12:26 +0100

    Change getClass to instanceof in equals method

--- api-layer/src/main/java/cz/fi/muni/pa165/dto/SportDTO.java
+++ api-layer/src/main/java/cz/fi/muni/pa165/dto/SportDTO.java
@@ -22,7 +22,7 @@ public class SportDTO {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof SportDTO)) return false;
 
         SportDTO sportDTO = (SportDTO) o;
 
