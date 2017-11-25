d0e0d43681338ac101201892df99487ac26d3b23
Martin Šmíd <martin.smid94@seznam.cz>
2017-11-25 13:30:58 +0100

    Add sport facade implementation

--- api-layer/src/main/java/cz/fi/muni/pa165/facade/SportFacade.java
+++ api-layer/src/main/java/cz/fi/muni/pa165/facade/SportFacade.java
@@ -1,11 +1,17 @@
 package cz.fi.muni.pa165.facade;
 
 import cz.fi.muni.pa165.dto.SportDTO;
-import cz.fi.muni.pa165.entity.Sport;
 
 /**
  * @author Martin Smid
  */
 public interface SportFacade extends BaseFacade<SportDTO> {
 
+    /**
+     * Create object by DTO params.
+     *
+     * @param sportDTO sport DTO
+     * @return ID of the object
+     */
+    Long create(SportDTO sportDTO);
 }
---
+++ service/src/main/java/cz/muni/fi/pa165/service/facade/SportFacadeImpl.java
@@ -0,0 +1,43 @@
+package cz.muni.fi.pa165.service.facade;
+
+import cz.fi.muni.pa165.dto.SportDTO;
+import cz.fi.muni.pa165.entity.Sport;
+import cz.fi.muni.pa165.facade.SportFacade;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.SportService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class SportFacadeImpl implements SportFacade {
+
+    @Autowired
+    private SportService sportService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public void delete(SportDTO dto) {
+        Sport sport = sportService.findById(dto.getId());
+        sportService.delete(sport);
+    }
+
+    @Override
+    public SportDTO load(Long id) {
+        return beanMappingService.mapTo(sportService.findById(id), SportDTO.class);
+    }
+
+    @Override
+    public List<SportDTO> getAll() {
+        return beanMappingService.mapTo(sportService.findAll(), SportDTO.class);
+    }
+
+    @Override
+    public Long create(SportDTO sportDTO) {
+        Sport sport = new Sport();
+        sport.setName(sportDTO.getName());
+        sportService.create(sport);
+        return sport.getId();
+    }
+}
