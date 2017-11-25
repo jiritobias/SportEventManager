65ca70e29b6a12dc5cd2044f9b0f9daab061c641
Martin Šmíd <martin.smid94@seznam.cz>
2017-11-25 15:35:50 +0100

    Fix imports

--- persistence/src/main/java/cz/fi/muni/pa165/entity/User.java
+++ persistence/src/main/java/cz/fi/muni/pa165/entity/User.java
@@ -5,10 +5,21 @@ import cz.fi.muni.pa165.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.*;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author jiritobias
