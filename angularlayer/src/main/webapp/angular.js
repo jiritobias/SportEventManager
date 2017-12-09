7df1b0b4517f7e31db3279aa73f72802016f50dc
jiritobias <tobi.jiri@gmail.com>
2017-12-09 15:37:51 +0100

    update

--- angularlayer/src/main/webapp/angular.js
+++ angularlayer/src/main/webapp/angular.js
@@ -6,3 +6,10 @@
 var pa165semApp = angular.module('pa165semApp', ['ngRoute', 'semControllers']);
 var semControllers = angular.module('semControllers', []);
 
+/* Configures URL fragment routing, e.g. #/product/1  */
+pa165semApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+        when('/sport', {templateUrl: 'partials/sport.html', controller: 'SportCtrl'}).
+        otherwise({redirectTo: '/error'});
+    }]);
