022f01af47552fa868529ff67e891888370d1584
Martin Šmíd <martin.smid94@seznam.cz>
2017-12-13 18:47:51 +0100

    Add limit to getUsers

--- web/src/main/java/cz/muni/fi/pa165/restapi/controllers/UserRestController.java
+++ web/src/main/java/cz/muni/fi/pa165/restapi/controllers/UserRestController.java
@@ -1,9 +1,12 @@
 package cz.muni.fi.pa165.restapi.controllers;
 
+import cz.fi.muni.pa165.dto.CreateSportsMenDTO;
 import cz.fi.muni.pa165.dto.SportsMenDTO;
 import cz.fi.muni.pa165.facade.SportsMenFacade;
 import cz.muni.fi.pa165.restapi.ApiUris;
 import cz.muni.fi.pa165.restapi.exceptions.InvalidParameterException;
+import cz.muni.fi.pa165.restapi.exceptions.InvalidRequestException;
+import cz.muni.fi.pa165.restapi.exceptions.ResourceAlreadyExistingException;
 import cz.muni.fi.pa165.restapi.exceptions.ResourceNotFoundException;
 import cz.muni.fi.pa165.restapi.hateoas.UserResource;
 import cz.muni.fi.pa165.restapi.hateoas.UserResourceAssembler;
@@ -12,18 +15,17 @@ import cz.muni.fi.pa165.restapi.hateoas.UserSimpleResourceAssembler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,7 +52,10 @@ public class UserRestController {
     private SportsMenFacade sportsMenFacade;
 
     @RequestMapping(method = RequestMethod.GET)
-    public final HttpEntity<Resources<UserResource>> getUsers(@RequestParam(value = "role", required = false, defaultValue = "SPORTSMEN") String role) {
+    public final HttpEntity<Resources<UserResource>> getUsers(
+            @RequestParam(value = "role", required = false, defaultValue = "SPORTSMEN") String role,
+            @RequestParam(value = "limit", required = false, defaultValue = "0") long limit
+            ) {
         logger.debug("UserRestController getUsers()");
 
         List<UserResource> userResources = new ArrayList<>();
@@ -65,6 +70,14 @@ public class UserRestController {
 
         } else {
             throw new InvalidParameterException("Role parameters options: user, admin, sportsmen, all");
+        }
+
+        if (limit > 0) {
+            List<UserResource> newResource = new ArrayList<>();
+            for (int i = 0; i < limit; i++) {
+                newResource.add(userResources.get(i));
+            }
+            userResources = newResource;
         }
 
         Resources<UserResource> resources = new Resources<>(
@@ -87,5 +100,24 @@ public class UserRestController {
 
         UserResource userResource = userResourceAssembler.toResource(sportsMenDTO);
         return new ResponseEntity<UserResource>(userResource, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final HttpEntity<UserResource> createUser(@RequestBody @Valid CreateSportsMenDTO sportsMenDTO, BindingResult bindingResult) {
+        logger.debug("UserRestController createUser()");
+
+        if (bindingResult.hasErrors()) {
+            logger.error("failed validation {}", bindingResult.toString());
+            throw new InvalidRequestException("Failed validation");
+        }
+
+        Long id;
+        try {
+            id = sportsMenFacade.createSportsMen(sportsMenDTO);
+        } catch (DataAccessException e) {
+            throw new ResourceAlreadyExistingException("User already exists");
+        }
+        UserResource resource = userResourceAssembler.toResource(sportsMenFacade.load(id));
+        return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 }
